8e9d2e6f915ac019408bc710bf772d310de64a87
Dicenice <dicenicepl@gmail.com>
2023-08-08 14:20:35 +0200

    nothing

--- src/main/resources/scripts/cookiesmanager.js
+++ src/main/resources/scripts/cookiesmanager.js
@@ -1,15 +1,15 @@
-const url = 'localhost:3000/auth/login?email=dicenicepl@gmail.com&password=1234';
-const request = new XMLHttpRequest();
-
-request.onload = function () {
-    if (request.status === 200) {
-        const data = request.responseText;
-
-        document.cookie = 'token=${data}';
-        console.log(data)
-    } else {
-        console.log('nieudane przypisanie tokenu')
-    }
-};
-request.open('GET', url);
-request.send();
+// const url = 'localhost:3000/auth/login?email=dicenicepl@gmail.com&password=1234';
+// const request = new XMLHttpRequest();
+//
+// request.onload = function () {
+//     if (request.status === 200) {
+//         const data = request.responseText;
+//
+//         document.cookie = 'token=${data}';
+//         console.log(data)
+//     } else {
+//         console.log('nieudane przypisanie tokenu')
+//     }
+// };
+// request.open('GET', url);
+// request.send();
