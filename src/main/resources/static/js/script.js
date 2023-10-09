d24890c88009e6e1e209a26699f8528ab4f86975
Dicenice <dicenicepl@gmail.com>
2023-10-09 16:17:49 +0200

    Testing scripts

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,6 +1,11 @@
 
 function test() {
-    const buttonsss = document.getElementById("button-test");
-    console.log(buttonsss.hidden)
-    console.log("hi")
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    if (email.length !== 0 && password.length !== 0){
+        console.log("email: " + email)
+        console.log("password: " + password)
+    }
+    document.getElementById("toChange").innerHTML = "Sup bro";
+
 }
