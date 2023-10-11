243bd6e0c0d8ccf556455854c28267a61beb8e7f
Dicenice <dicenicepl@gmail.com>
2023-10-11 23:25:59 +0200

    Creating cookies

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -9,17 +9,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function login_api_request(email, password) {
     const request = new XMLHttpRequest();
+
     request.open('POST', 'http://localhost:3000/api/auth/user/login/');
     request.setRequestHeader('Content-Type', 'application/json');
+
+    const data = JSON.stringify({ email: email, password: password });
+    request.send(data);
+
+
     request.onload = function () {
         if (request.status === 200) {
             alert('Zalogowano pomyślnie');
+            document.cookie = request.responseText;
+            console.log(document.cookie);
         } else {
             alert('Wystąpił błąd podczas logowania');
         }
     }
-
-    const data = JSON.stringify({ email: email, password: password });
-
-    request.send(data);
 }
