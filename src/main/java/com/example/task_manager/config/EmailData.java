8e63ee1ce4d23a27e795c23b7f16999d0cd6d0d5
Dicenicepl <81882014+Dicenicepl@users.noreply.github.com>
2023-11-05 21:16:35 +0100

    Create EmailData.java

---
+++ src/main/java/com/example/task_manager/config/EmailData.java
@@ -0,0 +1,11 @@
+package com.example.task_manager.config;
+
+public class EmailData {
+    public String host = "EMAIL HOST";
+    public int port = 1;
+    public String email = "EMAIL TO LOG IN TO SERVICE";
+    public String password = "PASSWORD TO LOG IN TO SERVICE";
+    // If you need this service you need to put your data, I can`t give you my data because
+    // I'm using my private email.
+    // Some email services need to activate POP3/IMAP.
+}
